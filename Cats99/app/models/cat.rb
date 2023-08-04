db87642006aab842ccbf83610174752e775fa35a
tanziti <tanzitian3@gmail.com>
2023-08-04 15:44:06 -0400

    first swtich

--- Cats99/app/models/cat.rb
+++ Cats99/app/models/cat.rb
@@ -1,3 +1,16 @@
 class Cat < ApplicationRecord
-  validates :birth_date, :color, :name, :sex presence: true
+  VALID_COLORS = %w(orange brown white black)
+  validates :birth_date, :color, :name, :sex, presence: true
+  validates :sex, inclusion: { in: %w(M F), message: "%{value} is not a valid sex" }
+  validates :color, inclusion: { in: VALID_COLORS, message: "%{value} is not a valid color"}
+  validate :birth_date_cannot_be_the_future 
+  
+
+
+  def birth_date_cannot_be_the_future
+    if birth_date > Date.today
+      errors.add(:birth_date, "can't be in the future")
+    end
+  end
+
 end
